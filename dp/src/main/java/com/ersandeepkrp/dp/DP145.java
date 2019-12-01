d5c115ba97100a936b13346cb0546bacf85a5205
markov <er.sandeep.krp@gmail.com>
2019-12-01 20:08:48 +0530

    Adds example to DP145

--- dp/src/main/java/com/ersandeepkrp/dp/DP145.java
+++ dp/src/main/java/com/ersandeepkrp/dp/DP145.java
@@ -6,6 +6,7 @@ import com.ersandeepkrp.dp.ds.Node;
  * 
  * @author sandeep-krp
  * Given the head of a singly linked list, swap every two nodes and return its head.
+ * For example, given 1 -> 2 -> 3 -> 4, return 2 -> 1 -> 4 -> 3
  *
  */
 public class DP145 {

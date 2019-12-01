5ffe75a091d9f92bd502ff3ed9c8a9cbc1fdc6fd
markov <er.sandeep.krp@gmail.com>
2019-12-01 14:31:43 +0530

    DP154 : Adds the problem statement to the code

--- dp/src/test/java/com/ersandeepkrp/dp/test/DP154Test.java
+++ dp/src/test/java/com/ersandeepkrp/dp/test/DP154Test.java
@@ -6,6 +6,16 @@ import org.junit.Test;
 
 import com.ersandeepkrp.dp.DP154;
 
+/**
+ * 
+ * @author sandeep-krp 
+ * Implement a stack API using only a heap. A stack
+ *         implements the following methods:
+ * 
+ *         push(item), which adds an element to the stack pop(), which removes
+ *         and returns the most recently added element (or throws an error if
+ *         there is nothing on the stack)
+ */
 public class DP154Test {
 
 	private DP154 solution;
@@ -37,7 +47,7 @@ public class DP154Test {
 		Assert.assertEquals(Integer.valueOf(8), solution.pop());
 		Assert.assertEquals(Integer.valueOf(7), solution.pop());
 	}
-	
+
 	@Test
 	public void test3() {
 		solution.push(3);
@@ -50,10 +60,9 @@ public class DP154Test {
 		Assert.assertEquals(Integer.valueOf(7), solution.pop());
 		Assert.assertEquals(Integer.valueOf(2), solution.pop());
 		Assert.assertEquals(Integer.valueOf(6), solution.pop());
-		
+
 	}
-	
-	
+
 	@Test
 	public void test4() {
 		solution.push(3);
@@ -68,7 +77,7 @@ public class DP154Test {
 		Assert.assertEquals(Integer.valueOf(6), solution.pop());
 		solution.push(20);
 		Assert.assertEquals(Integer.valueOf(20), solution.pop());
-		
+
 	}
-	
+
 }
